e10ba023ea30668182bdbc0effec3db803542527
julien colomb <julien.colomb@fu-berlin.de>
2024-12-06 15:29:42 +0100

    first try

---
+++ 91_Explanation_book/content/createglossary_yml.R
@@ -0,0 +1,28 @@
+library(readr)
+library(tidyverse)
+glossary <- read_csv("content/glossary.csv")
+
+for (i in c(1:nrow(glossary))){
+  element= as.character(glossary[i,1])
+  filename=
+    str_c( "content/",
+      sprintf("%03d", as.numeric(i)),
+      "_",     
+      str_replace_all(element, "[ /]", "_"),
+      ".qmd"
+    )
+  title =str_c("## ", element)
+  write_file(title,filename)
+  print(i)
+  }
+
+
+
+tableg=glossary_table(as_kable = FALSE)
+names (tableg) = c()
+
+
+as.list(tableg)
+
+textversion = nested_list(kl, type= "headers")
+print (textversion)
